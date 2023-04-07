8e971b4b4712daa62368a5cbd75a093e9e8ce7e9
nguyenming <nguyenming.6464@gmail.com>
2023-04-07 22:24:39 +0700

    finish lab

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret: "nodeauthsecret",
+  database:
+    "mongodb+srv://giaminh16092003:7rN6UlGBcmZm2Czu@mongotest.hdbc0el.mongodb.net/cp17305?retryWrites=true&w=majority",
+};
